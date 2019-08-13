72c28c8a538ee5a43f84b43019ce3bb64ad8819e
Sherwin Wu <sherwin.z.wu@gmail.com>
2019-08-12 22:21:41 -0700

    Render hard-coded ping points

--- src/StateMap.js
+++ src/StateMap.js
@@ -2,16 +2,36 @@ import React, { Component } from 'react'
 import './StateMap.css'
 import statedata from './data/us_states'
 import { geoAlbers, geoPath } from 'd3-geo'
+import { select } from 'd3-selection'
 
 class StateMap extends Component {
+  componentDidMount() {
+    const projection = geoAlbers();
+    const pathGenerator = geoPath().projection(projection);
+    const lat = 33.387583;
+    const long = -112.056912;
+    const data = [{'lat': 33.387583, 'long': -112.056912},
+                  {'lat': 38.601310, 'long': -121.389073},
+                  {'lat': 33.785792, 'long': -84.280779}];
+
+    select('.usMap')
+      .selectAll('circle')
+      .data(data)
+      .enter()
+      .append('circle')
+      .style('fill', '#1C85E8')
+      .attr('cx', d => projection([d.long, d.lat])[0])
+      .attr('cy', d => projection([d.long, d.lat])[1])
+      .attr('r', 10);
+  }
+
   render() {
     const projection = geoAlbers();
-    const pathGenerator = geoPath().projection(projection)
+    const pathGenerator = geoPath().projection(projection);
     const states = statedata.features
       .map((d, i) => <path key={'path' + i}
                            d={pathGenerator(d)}
-                           className='renderedState' />)
-
+                           className='renderedState' />);
 
     return <svg width={1080} height={500} className='usMap'>{states}</svg>
   }
