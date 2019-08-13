1bca04350e6fe436682d236c5810ce54958f87b9
Sherwin Wu <sherwin.z.wu@gmail.com>
2019-08-12 22:39:09 -0700

    Add animation to pings

--- src/StateMap.js
+++ src/StateMap.js
@@ -3,13 +3,11 @@ import './StateMap.css'
 import statedata from './data/us_states'
 import { geoAlbers, geoPath } from 'd3-geo'
 import { select } from 'd3-selection'
+import { transition } from 'd3-transition'
 
 class StateMap extends Component {
   componentDidMount() {
     const projection = geoAlbers();
-    const pathGenerator = geoPath().projection(projection);
-    const lat = 33.387583;
-    const long = -112.056912;
     const data = [{'lat': 33.387583, 'long': -112.056912},
                   {'lat': 38.601310, 'long': -121.389073},
                   {'lat': 33.785792, 'long': -84.280779}];
@@ -19,10 +17,14 @@ class StateMap extends Component {
       .data(data)
       .enter()
       .append('circle')
-      .style('fill', '#1C85E8')
+      .style('fill', '#1C85E8') // opendoor blue
       .attr('cx', d => projection([d.long, d.lat])[0])
       .attr('cy', d => projection([d.long, d.lat])[1])
-      .attr('r', 10);
+      .attr('r', 1)
+      .transition()
+        .duration(3000)
+        .attr('r', 40)
+        .style('fill', 'transparent');
   }
 
   render() {
