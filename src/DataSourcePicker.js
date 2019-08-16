bac262df037ddc65072f27360502e2b45f6b142a
Sherwin Wu <sherwin.z.wu@gmail.com>
2019-08-15 23:30:19 -0700

    Add hover and click select behavior to data source picker.

--- src/DataSourcePicker.js
+++ src/DataSourcePicker.js
@@ -1,24 +1,63 @@
 import React, { Component } from 'react'
 import './css/DataSourcePicker.css'
 
+class DataSource extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hover: false,
+      active: this.props.is_active
+    }
+  }
+
+  toggleHover() {
+    this.setState({hover: !this.state.hover});
+  }
+
+  onClick() {
+    this.setState({active: !this.state.active});
+  }
+
+  render() {
+    var activeDataSourceClass;
+    if (this.state.active) {
+      activeDataSourceClass = "ActiveDataSource";
+    } else {
+      activeDataSourceClass = "InactiveDataSource";
+    }
+
+    var dotColorClass = "OpendoorLightGrey";
+    if (this.state.active || this.state.hover) {
+      dotColorClass = this.props.dotColorClass;
+    }
+
+    return (
+      <div className={"DataSource " + activeDataSourceClass}
+           onMouseEnter={() => this.toggleHover()}
+           onMouseLeave={() => this.toggleHover()}
+           onClick={() => this.onClick()}>
+        {this.props.name}
+        <span className={"DataSourceDot " + dotColorClass}>&#9679;</span>
+      </div>
+    )
+  }
+}
+
 class DataSourcePicker extends Component {
   render() {
     return (
       <div className="DataSourcePicker">
-        <div className="DataSource">
-          Acquisition Offer Requests
-          <span className="DataSourceDot OpendoorBlue">&#9679;</span>
-        </div>
+        <DataSource is_active={true}
+                    dotColorClass="OpendoorBlue"
+                    name="Acquisition Offer Requests" />
 
-        <div className="DataSource">
-          Resale Offer Requests
-          <span className="DataSourceDot OpendoorRuby">&#9679;</span>
-        </div>
+        <DataSource is_active={false}
+                    dotColorClass="OpendoorRuby"
+                    name="Resale Offer Requests" />
 
-        <div className="DataSource">
-          Opendoor Home Visits
-          <span className="DataSourceDot OpendoorCitrine">&#9679;</span>
-        </div>
+        <DataSource is_active={false}
+                    dotColorClass="OpendoorCitrine"
+                    name="Opendoor Home Visits" />
       </div>
     )
   }
